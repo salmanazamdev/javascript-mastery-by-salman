42f0253b5c341ae59133af734ffc0c538f7f9f8a
SalmanAzamDev <salmankhach@gmail.com>
2025-07-14 12:05:23 +0500

    Fixed typo errors in Q7

--- 04_string_methods.js
+++ 04_string_methods.js
@@ -8,7 +8,6 @@ console.log("Lowercase:", lowerCaseString);
 
 
 // Q2: Extract the last 3 characters from the string "JavaScript" using slice().
-
 let isString = 'Javascript';
 let lastThreeChar = isString.slice(-3); // slice works in a way that it takes the last 3 characters from the string
 // Alternatively, you can use substring(isString.length - 3) to achieve the same result
