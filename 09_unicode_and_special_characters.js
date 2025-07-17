2f83d6b295ba5f6f8a1dccf5113db06d5867ce2d
SalmanAzamDev <salmankhach@gmail.com>
2025-07-17 14:50:48 +0500

    Initiated Unicode & Special Characters file

---
+++ 09_unicode_and_special_characters.js
@@ -0,0 +1,19 @@
+// 1. Print 5 emojis in a string and show their `.length` and `.codePointAt(0)`
+
+// 2. Convert "Hello" into Unicode escape sequences (\u0048...) and log it
+
+// 3. Print a multi-line string using \n and include a tab (\t) and carriage return (\r)
+
+// 4. Write a sentence that includes ©, ®, ™ and then remove them using .replace()
+
+// 5. Normalize "café" and check if it matches "café" using .normalize("NFC")
+
+// 6. Create an Urdu string, log the original text, and check its length
+
+// 7. Create a string with newline, tab, and emoji — explain output in comments
+
+// 8. Use `String.fromCodePoint()` to create a custom emoji string (codepoint-based)
+
+// 9. Get the last character’s code point from "💖😊🔥" and print it
+
+// 10. Split a string with multiple Unicode symbols using `.split('')` and count how many visible characters are shown
