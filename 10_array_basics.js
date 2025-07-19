7b63662e4117e00cd4f6699da46653432312940a
SalmanAzamDev <salmankhach@gmail.com>
2025-07-19 17:00:48 +0500

    Initial commit: Added Questions related to array basics

---
+++ 10_array_basics.js
@@ -0,0 +1,41 @@
+// Array Basics Exercises
+
+
+// 1. Create an array of 5 programming languages and print the 3rd one.
+
+
+
+// 2. Create an array of numbers. Print the first and last number using array indexes.
+
+
+
+// 3. Use the .length property to print how many fruits are in the array ["mango", "apple", "banana"].
+
+
+
+// 4. Use .at() to access the second last item from the array ["red", "blue", "green", "yellow"].
+
+
+
+// 5. Declare a mixed array containing a number, a string, a boolean, and null. Log the entire array.
+
+
+
+// 6. Access the value "JavaScript" from the following array:
+//let devStack = ["HTML", "CSS", "JavaScript", "React"];
+
+
+
+// 7. Create an empty array and push three values into it using .push()
+
+
+
+// 8. Remove the last element from an array using .pop() and print the updated array.
+
+
+
+// 9. Create an array of your 3 favorite sports. Replace the second sport with "Basketball".
+
+
+
+// 10. Create an array of city names and log the length of the array and the last city using .at(-1).
